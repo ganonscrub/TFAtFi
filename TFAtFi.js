56a3a7ba16c8a52b17aa0542e71b2aaad48ad4d4
Mike <mboharsik@gmail.com>
2016-02-21 10:41:07 -0500

    I guess it works

--- TFAtFi.js
+++ TFAtFi.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TimeForge Attendance Filler
 // @namespace    ganonscrub_script
-// @version      0.1
+// @version      0.2
 // @description  Makes TimeForge slightly less terrible
 // @author       ganonscrub
 // @include      *timeforge.com/Scheduler/sa/bulkEnterAttendance.html*
@@ -9,17 +9,17 @@
 // @updateURL	 https://raw.githubusercontent.com/ganonscrub/TFAtFi/master/TFAtFi.js
 // ==/UserScript==
 
-function Employee( name, position, start, end ){
-	if ( end == undefined || end == null )
+function Employee( name, start, end, position ){
+	if ( end === undefined || end === null )
 		end = "";
 	
-	this.name = name;
-	this.position = position;
+	this.name = name.toUpperCase();
 	this.start = start;
 	this.end = end;
+    this.position = position.toUpperCase();
 }
 
-Employees = [];
+var Employees;
 
 function getHighest(){
 	var i = 1;
@@ -37,7 +37,7 @@ function setName( nameBox, name ){
 	var names = nameBox.getElementsByTagName( "option" );
 	var hitvalue = null;
 	for ( var i = 0; i < names.length; i++ ){
-		if ( names[i].innerHTML.indexOf( name ) != -1 ){
+		if ( names[i].innerHTML.toUpperCase().indexOf( name ) != -1 ){
 			nameBox.value = names[i].value;
 			break;
 		}
@@ -48,47 +48,49 @@ function setPosition( positionBox, position ){
 	var positions = positionBox.getElementsByTagName( "option" );
 	var hitvalue = null;
 	for ( var i = 0; i < positions.length; i++ ){
-		if ( positions[i].innerHTML.indexOf( position ) != -1 ){
+        console.log( positions[i].innerHTML.toUpperCase(), position );
+		if ( positions[i].innerHTML.toUpperCase().indexOf( position ) != -1 ){
 			positionBox.value = positions[i].value;
-			break;
+			return;
 		}
 	}
 }
 
 function setTimeValue( timeBox, time ){
 	time = time.trim();
-	time = time.replace( "am", "AM" ).replace( "pm", "PM" );
+	time = time.replace( /am/g, "AM" ).replace( /pm/g, "PM" );
 	timeBox.value = time;
 }
 
-function parseEmployeeList(){
-	var driverString = "Driver: Night & Weekend Service";
+function getPosition( position ){
+    if ( position.toUpperCase().indexOf( "SUPERVISOR" ) != -1 )
+        return "Shift Supervisor";
+    else if ( position.toUpperCase().indexOf( "DISPATCHER" ) != - 1 )
+        return "Dispatcher";
+    else
+        return "Driver: Night &amp; Weekend Service";
+}
+
+function parseEmployeeList(){   
+    Employees = [];
+    
 	var includeEndTime = false;
 	var text = document.getElementById( "employeeDump" ).value;
-	text = text.trim().replace( /\n/g, "|" ).replace( /am/g, "AM" ).replace( /pm/g, "PM" );
+	text = text.trim().replace( /\n\n/g, "|" ).replace( /\n/g, "|" ).replace( /am/g, "AM" ).replace( /pm/g, "PM" );
 	tokens = text.split( "|" );
+    
+    // expect 5 tokens: name, late, start, end, route
 	
-	var length;
-	if ( includeEndTime )
-		length = tokens.length / 4;
-	else
-		length = tokens.length / 3;
-	
-	for ( var i = 0; i < length; i++ ){
-		var off;
-		if ( includeEndTime ){
-			off = i * 4;
-			Employees.push( new Employee( tokens[off], driverString, tokens[off+2], tokens[off+3] ) );
-		}
-		else{
-			off = i * 3 ;
-			Employees.push( new Employee( tokens[off], driverString, tokens[off+2] ) );
-		}
+	for ( var i = 0; i < tokens.length / 5; i++ ){
+        var off = i * 5;
+        Employees.push( new Employee( tokens[off], tokens[off+2], tokens[off+3], getPosition( tokens[off+4] ) ) );
 	}
+    
+    addEmployees();
 }
 
 function editCurrentRow(){
-	var employee = Employee.pop();
+	var employee = Employees.pop();
 	var n = getHighest();
 	var row = getHighestRow();
 	var nameSelect = document.getElementById( "emp" + n );
@@ -96,9 +98,11 @@ function editCurrentRow(){
 	var startBox = document.getElementById( "start" + n );
 	var endBox = document.getElementById( "end" + n );
 	
-	setName( nameSelect, "Hennessy, Michael" );
-	updatePositions( n );
-	setPosition( positionSelect, "Shift Supervisor" );
+	setName( nameSelect, employee.name );
+	updatePositions( n );    
+    
+	setPosition( positionSelect, employee.position );
+    
 	setTimeValue( startBox, employee.start );
 	setTimeValue( endBox, employee.end );
 	
@@ -113,6 +117,13 @@ function addEmployees(){
 
 var textarea = document.createElement( "textarea" );
 textarea.style.width = "800px";
-textarea.style.height = "400px";
+textarea.style.height = "32px";
 textarea.id = "employeeDump";
 document.body.appendChild( textarea );
+document.body.appendChild( document.createElement( "br" ) );
+
+var parsebutton = document.createElement( "input" );
+parsebutton.type = "button";
+parsebutton.value = "Parse Employees";
+parsebutton.addEventListener( "click", parseEmployeeList );
+document.body.appendChild( parsebutton );
